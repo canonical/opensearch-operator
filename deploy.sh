ba57476965e879462fd4d6751381e6ca567bda39
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2022-10-04 21:40:02 +0200

    Updated the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,32 @@
 #!/usr/bin/env bash
 
-juju destroy-model -y opensearch --no-wait --force --destroy-storage
+destroy=${1:-"false"}
 
-juju add-model opensearch
-juju switch :opensearch
+cat <<EOF > cloudinit-userdata.yaml
+cloudinit-userdata: |
+  postruncmd:
+    - [ "sysctl", "-w", "vm.swappiness=0" ]
+    - [ "sysctl", "-w", "vm.max_map_count=262144" ]
+    - [ "sysctl", "-w", "net.ipv4.tcp_retries2=5" ]
+EOF
+
+if [ "${destroy}" == "true" ]; then
+    juju destroy-model -y dev --no-wait --force --destroy-storage
+
+    juju add-model dev
+    juju switch :dev
+
+    juju deploy tls-certificates-operator --channel edge --show-log --verbose
+    juju config tls-certificates-operator generate-self-signed-certificates=true ca-common-name="CN_CA"
+else
+    juju switch :dev
+    juju remove-application opensearch --force
+fi
+
 juju model-config logging-config="<root>=INFO;unit=DEBUG"
+juju model-config ./cloudinit-userdata.yaml
 
 charmcraft pack
-
-juju deploy tls-certificates-operator --channel edge --show-log --verbose
 
 juju deploy -n 1 ./opensearch_ubuntu-22.04-amd64.charm --show-log --verbose
 
