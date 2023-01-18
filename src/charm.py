d62366904a4920d8893b5912af678b8e60f275ef
WRFitch <will.fitch@canonical.com>
2023-01-18 13:48:42 +0000

    update copyright notice

--- src/charm.py
+++ src/charm.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2022 Canonical Ltd.
+# Copyright 2023 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Charmed Machine Operator for OpenSearch."""
