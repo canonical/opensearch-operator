b85ee0edb8633067efaca28637b9293d5d60a926
WRFitch <will.fitch@canonical.com>
2023-02-02 11:51:49 +0000

    added some basic user management code for client relation

---
+++ lib/charms/opensearch/v0/opensearch_users.py
@@ -0,0 +1,158 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""OpenSearch user helpers.
+
+TODO update tests
+"""
+
+import logging
+from typing import Any, Dict, List
+
+from charms.opensearch.v0.opensearch_distro import OpenSearchDistribution
+
+logger = logging.getLogger(__name__)
+
+
+USER_ENDPOINT = "/_plugins/_security/api/internalusers"
+ROLE_ENDPOINT = "/_plugins/_security/api/roles"
+
+
+class OpenSearchUserMgmtError(Exception):
+    """Base exception class for OpenSearch user management errors."""
+
+
+def create_role(
+    opensearch: OpenSearchDistribution,
+    role_name: str,
+    permissions: Dict = {},
+    action_groups: Dict = {},
+) -> Dict[str, Any]:
+    """Creates a role with the given permissions.
+
+    This method assumes the dicts provided are valid opensearch config. If not, raises
+    OpenSearchUserMgmtError.
+
+    Args:
+        opensearch: Opensearch distribution object, used to send requests
+        role_name: name of the role
+        permissions: A valid dict of existing opensearch permissions.
+        action_groups: A valid dict of existing opensearch action groups.
+
+    Raises:
+        OpenSearchUserMgmtError: If the role creation request fails.
+
+    Returns:
+        Output of the role creation request.
+    """
+    put_role_resp = opensearch.request(
+        "PUT",
+        f"{ROLE_ENDPOINT}/{role_name}",
+        {**permissions, **action_groups},
+    )
+    # enable this role
+    logger.debug(put_role_resp)
+    if put_role_resp.get("status") != "OK":
+        raise OpenSearchUserMgmtError()
+    return put_role_resp
+
+
+def remove_role(opensearch: OpenSearchDistribution, role_name: str) -> Dict[str, Any]:
+    """Remove the given role from opensearch distribution.
+
+    Args:
+        opensearch: Opensearch distribution object, used to send requests
+        role_name: name of the role to be removed.
+
+    Raises:
+        OpenSearchUserMgmtError: If the request fails.
+
+    Returns:
+        Output of the request.
+    """
+    resp = opensearch.request("DELETE", f"{ROLE_ENDPOINT}/{role_name}")
+    logger.debug(resp)
+    # check if I have to disable roles before removal
+    if resp.get("status") != "OK":
+        raise OpenSearchUserMgmtError()
+    return resp
+
+
+def create_user(
+    opensearch: OpenSearchDistribution,
+    username: str,
+    roles: List[str],  # TODO add default
+    hashed_pwd: str,
+) -> Dict[str, Any]:
+    """Create or update user and assign the requested roles to the user.
+
+    Args:
+        opensearch: Opensearch distribution object, used to send requests
+        username: name of the user to be created.
+        roles: list of roles to be applied to the user. These must already exist.
+        hashed_pwd: the hashed password for the user.
+
+    Raises:
+        OpenSearchUserMgmtError: If the request fails.
+
+    Returns:
+        Output of the request.
+    """
+    payload = {"hash": hashed_pwd}
+    if roles:
+        payload["opendistro_security_roles"] = roles
+
+    put_user_resp = opensearch.request(
+        "PUT",
+        f"{USER_ENDPOINT}/{username}",
+        payload,
+    )
+    logger.debug(put_user_resp)
+    if put_user_resp.get("status") != "CREATED":
+        raise OpenSearchUserMgmtError()
+    return put_user_resp
+
+
+def remove_user(opensearch: OpenSearchDistribution, username: str) -> Dict[str, Any]:
+    """Remove the given user from opensearch distribution.
+
+    Args:
+        opensearch: Opensearch distribution object, used to send requests
+        username: name of the user to be removed.
+
+    Raises:
+        OpenSearchUserMgmtError: If the request fails.
+
+    Returns:
+        Output of the request.
+    """
+    resp = opensearch.request("DELETE", f"{USER_ENDPOINT}/{username}/")
+    logger.debug(resp)
+    if resp.get("status") != "OK":
+        raise OpenSearchUserMgmtError()
+    return resp
+
+
+def patch_user(opensearch, user: str, patches: List[Dict[str, any]]) -> Dict[str, Any]:
+    """Applies patches to user.
+
+    Args:
+        opensearch: Opensearch distribution object, used to send requests
+        user: name of the user to be created.
+        patches: a list of patches to be applied to the user in question.
+
+    Raises:
+        OpenSearchUserMgmtError: If the request fails.
+
+    Returns:
+        Output of the request.
+    """
+    patch_user_resp = opensearch.request(
+        "PATCH",
+        f"{USER_ENDPOINT}/{user}",
+        patches,
+    )
+    logger.debug(patch_user_resp)
+    if patch_user_resp.get("status") != "OK":
+        raise OpenSearchUserMgmtError()
+    return patch_user_resp
---
+++ tests/unit/lib/test_opensearch_users.py
@@ -0,0 +1,117 @@
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Unit tests for the opensearch_users library."""
+
+import unittest
+from unittest.mock import MagicMock
+
+import pytest
+from charms.opensearch.v0.opensearch_users import (
+    OpenSearchUserMgmtError,
+    create_role,
+    create_user,
+    patch_user,
+    remove_role,
+    remove_user,
+)
+
+from tests.helpers import patch_network_get
+
+
+@patch_network_get("1.1.1.1")
+class TestOpenSearchUsers(unittest.TestCase):
+    def test_create_role(self):
+        opensearch = MagicMock()
+        opensearch.request.return_value = {"status": "not ok"}
+        permissions = {"perm1": "gimme_perms"}
+        action_groups = {"ag1": "gimme_more_perms"}
+        role_kwargs = {
+            "opensearch": opensearch,
+            "role_name": "role_name",
+            "permissions": permissions,
+            "action_groups": action_groups,
+        }
+
+        with pytest.raises(OpenSearchUserMgmtError):
+            create_role(**role_kwargs)
+        request_args = (
+            "PUT",
+            "/_plugins/_security/api/roles/role_name",
+            {**permissions, **action_groups},
+        )
+        opensearch.request.assert_called_with(*request_args)
+
+        opensearch.request.reset_mock()
+        opensearch.request.return_value = {"status": "OK"}
+        create_role(**role_kwargs)
+        opensearch.request.assert_called_with(*request_args)
+
+    def test_remove_role(self):
+        opensearch = MagicMock()
+        opensearch.request.return_value = {"status": "not ok"}
+        role_args = (opensearch, "role_name")
+        with pytest.raises(OpenSearchUserMgmtError):
+            remove_role(*role_args)
+        request_args = ("DELETE", "/_plugins/_security/api/roles/role_name")
+        opensearch.request.assert_called_with(*request_args)
+
+        opensearch.request.reset_mock()
+        opensearch.request.return_value = {"status": "OK"}
+        remove_role(*role_args)
+        opensearch.request.assert_called_with(*request_args)
+
+    def test_create_user(self):
+        opensearch = MagicMock()
+        opensearch.request.return_value = {"status": "not ok"}
+        roles = ["my_cool_role", "my_terrible_role"]
+        hash_pw = "pw"
+        user_kwargs = {
+            "opensearch": opensearch,
+            "username": "username",
+            "roles": roles,
+            "hashed_pwd": hash_pw,
+        }
+
+        with pytest.raises(OpenSearchUserMgmtError):
+            create_user(**user_kwargs)
+        request_args = (
+            "PUT",
+            "/_plugins/_security/api/internalusers/username",
+            {"hash": hash_pw, "opendistro_security_roles": roles},
+        )
+        opensearch.request.assert_called_with(*request_args)
+
+        opensearch.request.reset_mock()
+        opensearch.request.return_value = {"status": "CREATED"}
+        create_user(**user_kwargs)
+        opensearch.request.assert_called_with(*request_args)
+
+    def test_remove_user(self):
+        opensearch = MagicMock()
+        opensearch.request.return_value = {"status": "not ok"}
+        user_args = (opensearch, "username")
+        with pytest.raises(OpenSearchUserMgmtError):
+            remove_user(*user_args)
+        request_args = ("DELETE", "/_plugins/_security/api/internalusers/username/")
+        opensearch.request.assert_called_with(*request_args)
+
+        opensearch.request.reset_mock()
+        opensearch.request.return_value = {"status": "OK"}
+        remove_user(*user_args)
+        opensearch.request.assert_called_with(*request_args)
+
+    def test_patch_user(self):
+        opensearch = MagicMock()
+        opensearch.request.return_value = {"status": "not ok"}
+        patches = [{"test patch": "yep, looks like a test."}]
+        patch_args = (opensearch, "username", patches)
+        with pytest.raises(OpenSearchUserMgmtError):
+            patch_user(*patch_args)
+        request_args = ("PATCH", "/_plugins/_security/api/internalusers/username", patches)
+        opensearch.request.assert_called_with(*request_args)
+
+        opensearch.request.reset_mock()
+        opensearch.request.return_value = {"status": "OK"}
+        patch_user(*patch_args)
+        opensearch.request.assert_called_with(*request_args)
