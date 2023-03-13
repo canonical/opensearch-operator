f795917659ebc19a14b250305894c70a1eec0a9e
WRFitch <will.fitch@canonical.com>
2023-03-13 15:19:05 +0000

    lint

--- lib/charms/opensearch/v0/opensearch_exceptions.py
+++ lib/charms/opensearch/v0/opensearch_exceptions.py
@@ -69,7 +69,7 @@ class OpenSearchHttpError(OpenSearchError):
     def __init__(self, response_body: Optional[str] = None, response_code: Optional[int] = None):
         try:
             self.response_body = json.loads(response_body)
-        except:
+        except json.JSONDecodeError:
             self.response_body = None
         self.response_code = response_code
 
