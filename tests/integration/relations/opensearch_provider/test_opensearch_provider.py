986ec7ab87be2c37653ef12dd04e3b3ca4d27011
WRFitch <will.fitch@canonical.com>
2023-04-25 15:45:59 +0100

    deploy stable TLS operator

--- tests/integration/relations/opensearch_provider/test_opensearch_provider.py
+++ tests/integration/relations/opensearch_provider/test_opensearch_provider.py
@@ -65,7 +65,7 @@ async def test_create_relation(ops_test: OpsTest, application_charm, opensearch_
             num_units=NUM_UNITS,
             series=SERIES,
         ),
-        ops_test.model.deploy(TLS_CERTIFICATES_APP_NAME, channel="edge", config=tls_config),
+        ops_test.model.deploy(TLS_CERTIFICATES_APP_NAME, channel="stable", config=tls_config),
     )
     await ops_test.model.relate(OPENSEARCH_APP_NAME, TLS_CERTIFICATES_APP_NAME)
     wait_for_relation_joined_between(ops_test, OPENSEARCH_APP_NAME, TLS_CERTIFICATES_APP_NAME)
