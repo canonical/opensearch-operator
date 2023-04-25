6bd3167db11322eec03f00e39f7dd29b4d7689c7
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2023-04-25 11:59:39 +0200

    Fixed retry logic

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -4,7 +4,6 @@
 import json
 import logging
 import tempfile
-from datetime import datetime
 from pathlib import Path
 from types import SimpleNamespace
 from typing import Dict, List, Optional, Union
@@ -391,15 +390,10 @@ async def check_cluster_formation_successful(
 async def is_up(ops_test: OpsTest, unit_ip: str, retries: int = 15) -> bool:
     """Return if node up."""
     try:
-        for attempt in Retrying(
-            stop=stop_after_attempt(retries), wait=wait_fixed(wait=10) + wait_random(0, 5)
-        ):
+        for attempt in Retrying(stop=stop_after_attempt(retries), wait=wait_fixed(wait=5)):
             with attempt:
-                logger.info(f"is_up attempt for {unit_ip}: {datetime.now().strftime('%H:%M:%S')}")
-                http_resp_code = await http_request(
-                    ops_test, "GET", f"https://{unit_ip}:9200/", resp_status_code=True
-                )
-                return http_resp_code == 200
+                await http_request(ops_test, "GET", f"https://{unit_ip}:9200/")
+                return True
     except RetryError:
         return False
 
