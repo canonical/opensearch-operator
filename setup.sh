e711ce455e558fceb7dc2e0553e0dcf24b1075cc
WRFitch <will.fitch@canonical.com>
2023-05-11 10:58:29 +0000

    added test job to CI

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "running setup.sh"
 
 sudo sysctl -w vm.max_map_count=262144 vm.swappiness=0 net.ipv4.tcp_retries2=5
-export JUJU_VER=$(juju version | cut -c1-1)
+export JUJU_VERSION=$(juju version | cut -c1-1)
+echo "juju major version is $JUJU_VERSION"
 
 echo "setup.sh completed"
