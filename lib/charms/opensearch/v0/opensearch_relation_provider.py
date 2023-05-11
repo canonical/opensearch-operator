efb55bd1d0d74022b5ab70b0f3eeeff6f3f556bb
WRFitch <will.fitch@canonical.com>
2023-05-11 14:15:15 +0000

    lint

--- lib/charms/opensearch/v0/opensearch_relation_provider.py
+++ lib/charms/opensearch/v0/opensearch_relation_provider.py
@@ -189,9 +189,7 @@ class OpenSearchProvider(Object):
                 and e.response_body.get("error", {}).get("type")
                 == "resource_already_exists_exception"
             ):
-                failed_to_create = (
-                    f"failed to create {event.index} index due to {e} - deferring index-requested event..."
-                )
+                failed_to_create = f"failed to create {event.index} index due to {e} - deferring index-requested event..."
                 logger.error(failed_to_create)
                 self.unit.status = BlockedStatus(failed_to_create)
                 event.defer()
