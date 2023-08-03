c504fd11b087d30b70a153a1ec9cc12fa9239c0b
Judit Novak <judit.novak@canonical.com>
2023-08-03 16:39:31 +0200

    Removing all() method from [Relation]DataStore

--- lib/charms/opensearch/v0/opensearch_internal_data.py
+++ lib/charms/opensearch/v0/opensearch_internal_data.py
@@ -53,11 +53,6 @@ class DataStore(ABC):
         pass
 
     @abstractmethod
-    def all(self, scope: Scope) -> Dict[str, str]:
-        """Get all content of a store."""
-        pass
-
-    @abstractmethod
     def get(
         self, scope: Scope, key: str, default: Optional[Union[int, float, str, bool]] = None
     ) -> Optional[Union[int, float, str, bool]]:
@@ -138,14 +133,6 @@ class RelationDataStore(DataStore):
             raise ValueError("Scope undefined.")
 
         return key in self._get_relation_data(scope)
-
-    @override
-    def all(self, scope: Scope) -> Dict[str, str]:
-        """Get all content of a store."""
-        if scope is None:
-            raise ValueError("Scope undefined.")
-
-        return self._get_relation_data(scope)
 
     @override
     def get(
