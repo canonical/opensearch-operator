8c220476218078d5d2cf2046473ca350e0ea4606
Carl Csaposs <carl.csaposs@canonical.com>
2024-04-12 15:52:24 +0200

    Handle leader raising uncaught exception while acquiring peer databag lock
    If the leader unit raises an uncaught exception in the same Juju event that it acquires the peer databag lock, the changes to the peer databag will be reverted—which could allow another unit to get the lock.
    
    Therefore, we should wait until the next Juju event to use the lock.
    
    Note: for the leader unit, instead of the lock being requested & granted in the same event, the process is now:
    - leader unit requests lock & provisionally grants itself lock
    - non-leader units get peer-relation-changed and put a unique value in their databag to trigger peer-relation-changed on leader
    - leader gets peer-relation-changed (or any other event) and proceeds to use lock and start/stop opensearch
    
    Context: https://matrix.to/#/!xdClnUGkurzjxqiQcN:ubuntu.com/$dRO72RvyFD4EI65_n83hIQPAMFtHmVOQtjG5bMENaMk?via=ubuntu.com&via=matrix.org&via=cutefunny.art

--- lib/charms/opensearch/v0/opensearch_locking.py
+++ lib/charms/opensearch/v0/opensearch_locking.py
@@ -4,6 +4,7 @@
 """Ensure that only one node (re)starts, joins the cluster, or leaves the cluster at a time."""
 import json
 import logging
+import os
 import typing
 
 import ops
@@ -84,18 +85,52 @@ class _PeerRelationLock(ops.Object):
 
     @property
     def _unit_with_lock(self) -> str | None:
-        if self._relation:
-            return self._relation.data[self._charm.app].get("unit-with-lock")
+        if self._relation and (unit := self._relation.data[self._charm.app].get("unit-with-lock")):
+            if (
+                self._charm.unit.is_leader()
+                and self._relation.data[self._charm.app][
+                    "leader-acquired-lock-after-juju-event-id"
+                ]
+                == os.environ["JUJU_CONTEXT_ID"]
+            ):
+                # `unit-with-lock` was set in this Juju event
+                # If the charm code raises an uncaught exception later in the Juju event,
+                # `unit-with-lock` will be reverted to its previous value—which could allow another
+                # unit to get the lock.
+                # Therefore, we cannot use the lock now. We must wait until the next Juju event,
+                # when `unit-with-lock` has been committed (i.e. won't be reverted), to use the
+                # lock.
+                logger.debug(
+                    "[Node lock] Waiting until next Juju event to use peer databag lock for leader unit"
+                )
+                return
+            return unit
 
     @_unit_with_lock.setter
     def _unit_with_lock(self, value: str):
         assert self._relation
+        assert self._relation.data[self._charm.app].get("unit-with-lock") != value
+        if value == self._charm.unit.name:
+            logger.debug("[Node lock] (leader) granted peer lock to own unit")
+            # Prevent leader unit from using lock in the same Juju event that it was granted
+            # If the charm code raises an uncaught exception later in the Juju event,
+            # `unit-with-lock` will be reverted to its previous value—which could allow another
+            # unit to get the lock.
+            # Therefore, we cannot use the lock in this Juju event. We must wait until the next
+            # Juju event, when `unit-with-lock` has been committed (i.e. won't be reverted), to use
+            # the lock.
+            # `JUJU_CONTEXT_ID` is unique for each Juju event
+            # (https://matrix.to/#/!xdClnUGkurzjxqiQcN:ubuntu.com/$yEGjGlDaIPBtCi8uB3fH6ZaXUjN7GF-Y2s9YwvtPM-o?via=ubuntu.com&via=matrix.org&via=cutefunny.art)
+            self._relation.data[self._charm.app][
+                "leader-acquired-lock-after-juju-event-id"
+            ] = os.environ["JUJU_CONTEXT_ID"]
         self._relation.data[self._charm.app]["unit-with-lock"] = value
 
     @_unit_with_lock.deleter
     def _unit_with_lock(self):
         assert self._relation
         self._relation.data[self._charm.app].pop("unit-with-lock", None)
+        self._relation.data[self._charm.app].pop("leader-acquired-lock-after-juju-event-id", None)
 
     @property
     def _relation(self):
@@ -105,17 +140,30 @@ class _PeerRelationLock(ops.Object):
 
     def _on_peer_relation_changed(self, _=None):
         """Grant & release lock."""
-        if not self._charm.unit.is_leader():
-            return
         assert self._relation
+        if not self._charm.unit.is_leader():
+            if self._relation.data[self._charm.app].get(
+                "leader-acquired-lock-after-juju-event-id"
+            ):
+                # Trigger peer relation changed event on leader unit
+                # Without this, the leader unit might not receive another event (to use the lock it
+                # holds) until the next update status event
+                # Use `JUJU_CONTEXT_ID` only to ensure that the value changes
+                # (Value should never be read)
+                # (If we set the same value that is currently in the databag, a peer relation
+                # changed event will not be triggered)
+                self._relation.data[self._charm.unit]["_trigger"] = os.environ["JUJU_CONTEXT_ID"]
+            return
         if self._unit_with_lock and self._unit_requested_lock(
             self._charm.model.get_unit(self._unit_with_lock)
         ):
             # Lock still in use, do not release
             logger.debug("[Node lock] (leader) lock still in use")
             return
-        # TODO: adjust which unit gets priority on lock?
-        for unit in (*self._relation.units, self._charm.unit):
+        # TODO: adjust which unit gets priority on lock after leader?
+        # During initial startup, leader unit must start first
+        # Give priority to leader unit
+        for unit in (self._charm.unit, *self._relation.units):
             if self._unit_requested_lock(unit):
                 self._unit_with_lock = unit.name
                 logger.debug(f"[Node lock] (leader) granted peer lock to {unit.name=}")
