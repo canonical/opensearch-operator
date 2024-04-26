1c3ecefbcf1b98d537de85256622c82db9dd9d33
Carl Csaposs <carl.csaposs@canonical.com>
2024-04-26 16:14:38 +0200

    Don't return upgrade app status if upgrade in progress
    Ported from https://github.com/canonical/mysql-router-operator/pull/141

--- src/machine_upgrade.py
+++ src/machine_upgrade.py
@@ -54,6 +54,8 @@ class Upgrade(upgrade.Upgrade):
     @property
     def app_status(self) -> typing.Optional[ops.StatusBase]:
         """App upgrade status"""
+        if not self.in_progress:
+            return
         if not self.is_compatible:
             logger.info(
                 "Upgrade incompatible. If you accept potential *data loss* and *downtime*, you can continue by running `force-upgrade` action on each remaining unit"
