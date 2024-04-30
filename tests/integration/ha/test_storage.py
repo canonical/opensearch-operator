58da533ef466cefd2afc923fc86c5628ece3f7b2
reneradoi <rene.radoi@canonical.com>
2024-04-30 09:36:45 +0000

    test_storage.py: app status will not be active because after scaling down not all shards are assigned

--- tests/integration/ha/test_storage.py
+++ tests/integration/ha/test_storage.py
@@ -72,7 +72,8 @@ async def test_storage_reuse_after_scale_down(
     # scale-down to 1
     await ops_test.model.applications[app].destroy_unit(f"{app}/{unit_id}")
     await ops_test.model.wait_for_idle(
-        apps=[app], status="active", timeout=1000, wait_for_exact_units=1
+        # app status will not be active because after scaling down not all shards are assigned
+        apps=[app], timeout=1000, wait_for_exact_units=1, idle_period=IDLE_PERIOD
     )
 
     # add unit with storage attached
