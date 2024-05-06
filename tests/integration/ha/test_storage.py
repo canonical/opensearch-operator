b19faf30bed83eeabd77ccb259be405840169caf
reneradoi <rene.radoi@canonical.com>
2024-05-06 08:06:46 +0000

    test_storage.py: continue writing data to check opensearch availability

--- tests/integration/ha/test_storage.py
+++ tests/integration/ha/test_storage.py
@@ -10,7 +10,13 @@ import time
 import pytest
 from pytest_operator.plugin import OpsTest
 
-from ..ha.helpers import app_name, storage_id, storage_type
+from ..ha.helpers import (
+    app_name,
+    assert_continuous_writes_consistency,
+    assert_continuous_writes_increasing,
+    storage_id,
+    storage_type,
+)
 from ..ha.test_horizontal_scaling import IDLE_PERIOD
 from ..helpers import APP_NAME, MODEL_CONFIG, SERIES, get_application_unit_ids
 from ..tls.test_tls import TLS_CERTIFICATES_APP_NAME
@@ -168,6 +174,13 @@ async def test_storage_reuse_after_scale_to_zero(
     # check if data is also imported
     assert writes_result.count == (await c_writes.count())
     assert writes_result.max_stored_id == (await c_writes.max_stored_id())
+
+    # Restart the writes, so we can validate the cluster is still working
+    c_writes = ContinuousWrites(ops_test, app, initial_count=writes_result.count)
+    await c_writes.start()
+    await assert_continuous_writes_increasing(c_writes)
+    # final validation
+    await assert_continuous_writes_consistency(ops_test, c_writes, app)
 
 
 @pytest.mark.group(1)
