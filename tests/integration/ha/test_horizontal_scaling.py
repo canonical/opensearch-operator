de6645cfb635ca5fde15892a409da2c7569c565d
Carl Csaposs <carl.csaposs@canonical.com>
2024-05-08 11:39:02 +0200

    fix collection

---
+++ tests/integration/ha/test_horizontal_scaling.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+from ..helpers import (
+    IDLE_PERIOD,
+)
---
+++ tests/integration/tls/test_tls.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# Copyright 2023 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+TLS_CERTIFICATES_APP_NAME = "self-signed-certificates"
