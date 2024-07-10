ce0c3e61d22e3ba3f2e709d925dc6bc37ac30097
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-07-10 06:26:01 +0200

    Add explain call to update status

--- lib/charms/opensearch/v0/opensearch_base_charm.py
+++ lib/charms/opensearch/v0/opensearch_base_charm.py
@@ -532,6 +532,11 @@ class OpenSearchBaseCharm(CharmBase, abc.ABC):
             without the user noticing in case the cert of the unit transport layer expires.
             So we want to stop opensearch in that case, since it cannot be recovered from.
         """
+        if self.opensearch.is_node_up():
+            logger.debug(
+                f'Current shard allocation status: {self.opensearch.request("GET", "/_cluster/allocation/explain")}'
+            )
+
         # if there are missing system requirements defer
         if len(missing_sys_reqs := self.opensearch.missing_sys_requirements()) > 0:
             self.status.set(BlockedStatus(" - ".join(missing_sys_reqs)))
