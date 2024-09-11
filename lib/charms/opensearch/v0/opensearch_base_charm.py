06c7d2526c9454b01196bb59486a8225a967d5a4
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-09-11 13:38:49 +0200

    Fix comment from last commit

--- lib/charms/opensearch/v0/opensearch_base_charm.py
+++ lib/charms/opensearch/v0/opensearch_base_charm.py
@@ -323,7 +323,7 @@ class OpenSearchBaseCharm(CharmBase, abc.ABC):
             and "cluster_manager" in self.opensearch.roles
             and not self.opensearch.is_service_started()
         ):
-            # This logic will only be triggered if the service has started (i.e. marked as "started")
+            # This logic will only be triggered if the service has started (i.e. "started")
             # if we had a "start" hook (i.e. the actual machine has rebooted)
             # and we are a cluster_manager with the service down
             # After these conditions are met, then we can simply restart the service.
