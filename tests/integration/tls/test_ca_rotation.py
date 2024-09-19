ba7bc12296cfc4def2284d8db067b4a46ea9e9c4
reneradoi <rene.radoi@canonical.com>
2024-09-19 17:03:18 +0000

    fix lint

--- tests/integration/tls/test_ca_rotation.py
+++ tests/integration/tls/test_ca_rotation.py
@@ -137,7 +137,11 @@ async def test_build_large_deployment(ops_test: OpsTest) -> None:
             application_name=FAILOVER_APP,
             num_units=1,
             series=SERIES,
-            config={"cluster_name": CLUSTER_NAME, "init_hold": True, "roles": "cluster_manager,data"},
+            config={
+                "cluster_name": CLUSTER_NAME,
+                "init_hold": True,
+                "roles": "cluster_manager,data",
+            },
         ),
         ops_test.model.deploy(
             my_charm,
