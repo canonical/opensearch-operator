490c6747f3457b0ebf2ba4a945c2ea3f3022e3e1
reneradoi <rene.radoi@canonical.com>
2024-10-18 13:25:00 +0000

    adjust integration test workflow to first scale down to one remaining unit before removing the application

--- tests/integration/ha/test_storage.py
+++ tests/integration/ha/test_storage.py
@@ -251,15 +251,37 @@ async def test_storage_reuse_in_new_cluster_after_app_removal(
 
     writes_result = await c_writes.stop()
 
-    # get unit info
+    # Scale down carefully to be able to identify which storage needs to be deployed to
+    # the leader when scaling up again. This is to avoid stale metadata when re-using the
+    # storage on a different cluster.
     storage_ids = []
-    for unit_id in get_application_unit_ids(ops_test, app):
+    unit_ids = get_application_unit_ids(ops_test, app)
+
+    # remember the current storage disks
+    for unit_id in unit_ids:
         storage_ids.append(storage_id(ops_test, app, unit_id))
 
-    # remove the remaining application
+    # remove all but the first unit
+    # this will trigger the remaining unit to become the leader if it wasn't already
+    for unit_id in unit_ids[1:]:
+        await ops_test.model.applications[app].destroy_unit(f"{app}/{unit_id}")
+
+    await wait_until(
+        ops_test,
+        apps=[app],
+        apps_statuses=["active"],
+        units_statuses=["active"],
+        timeout=1000,
+        idle_period=IDLE_PERIOD,
+        wait_for_exact_units={
+            app: 1,
+        },
+    )
+
+    # remove the remaining unit and the entire application
     await ops_test.model.remove_application(app, block_until_done=True)
 
-    # deploy new cluster
+    # deploy new cluster, attaching the storage from the previous leader to the new leader
     my_charm = await ops_test.build_charm(".")
     deploy_cluster_with_storage_cmd = (
         f"deploy {my_charm} --model={ops_test.model.info.name} --attach-storage={storage_ids[0]}"
