c950c92210b3312376fe33a9a9ed57a00ca0f1ad
Pedro Guimaraes <pedro.guimaraes@canonical.com>
2024-12-08 13:15:22 +0100

    Fix merge conflicts in test_plugins.py (2)

--- tests/integration/plugins/test_plugins.py
+++ tests/integration/plugins/test_plugins.py
@@ -221,30 +221,11 @@ async def test_config_switch_before_cluster_ready(ops_test: OpsTest, deploy_type
         idle_period=IDLE_PERIOD,
     )
     await assert_knn_config_updated(ops_test, True, check_api=False)
-
-    # Relate it to OpenSearch to set up TLS.
-    await ops_test.model.integrate(APP_NAME, TLS_CERTIFICATES_APP_NAME)
-    await _wait_for_units(ops_test, deploy_type)
-    assert len(ops_test.model.applications[APP_NAME].units) == 3
-
-
-@pytest.mark.parametrize("deploy_type", SMALL_DEPLOYMENTS)
-@pytest.mark.abort_on_fail
-async def test_config_switch_before_cluster_ready(ops_test: OpsTest, deploy_type) -> None:
-    """Configuration change before cluster is ready.
-
-    We hold the cluster without starting its unit services by not relating to tls-operator.
-    """
-    await ops_test.model.applications[APP_NAME].set_config({"plugin_opensearch_knn": "true"})
-    await wait_until(
-        ops_test,
-        apps=[APP_NAME],
-        units_statuses=["blocked"],
-        wait_for_exact_units={APP_NAME: 3},
-        timeout=3400,
-        idle_period=IDLE_PERIOD,
+    # Deploy TLS Certificates operator.
+    config = {"ca-common-name": "CN_CA"}
+    await asyncio.gather(
+        ops_test.model.deploy(TLS_CERTIFICATES_APP_NAME, channel="stable", config=config),
     )
-    await assert_knn_config_updated(ops_test, True, check_api=False)
 
 
 @pytest.mark.parametrize("deploy_type", SMALL_DEPLOYMENTS)
