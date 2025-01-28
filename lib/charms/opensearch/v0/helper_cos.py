df886259ea72545129b5ce0a6070f95d8da7721b
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2025-01-28 18:43:09 -0300

    - remove support for more than one json file and try except for   files that are not well formatted.

--- lib/charms/opensearch/v0/helper_cos.py
+++ lib/charms/opensearch/v0/helper_cos.py
@@ -5,7 +5,6 @@
 
 import json
 import logging
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 from data_platform_helpers.version_check import get_charm_revision
@@ -16,23 +15,11 @@ if TYPE_CHECKING:
     from charms.opensearch.v0.opensearch_base_charm import OpenSearchBaseCharm
 
 
-def update_grafana_dashboards_titles(charm: "OpenSearchBaseCharm") -> None:
-    """Update the titles in the specified directory to include the charm revision."""
+def update_grafana_dashboards_title(charm: "OpenSearchBaseCharm") -> None:
+    """Update the title of the Grafana dashboard file to include the charm revision."""
     revision = get_charm_revision(charm.model.unit)
-    path = charm.charm_dir / "src/grafana_dashboards"
+    dashboard_path = charm.charm_dir / "src/grafana_dashboards/opensearch.json"
 
-    for dashboard_path in path.iterdir():
-        if dashboard_path.is_file() and dashboard_path.suffix == ".json":
-            try:
-                _update_dashboard_title(revision, dashboard_path)
-            except (json.JSONDecodeError, IOError) as e:
-                logger.error("Failed to process %s: %s", dashboard_path.name, str(e))
-        else:
-            logger.warning("%s is not a valid JSON file", dashboard_path.name)
-
-
-def _update_dashboard_title(revision: str, dashboard_path: Path) -> None:
-    """Update the title of a Grafana dashboard file to include the charm revision."""
     with open(dashboard_path, "r") as file:
         dashboard = json.load(file)
 
--- tests/unit/lib/test_helper_cos.py
+++ tests/unit/lib/test_helper_cos.py
@@ -6,125 +6,75 @@
 import json
 import unittest
 from pathlib import Path
-from unittest.mock import MagicMock, PropertyMock, call, mock_open, patch
+from unittest.mock import MagicMock, PropertyMock, mock_open, patch
 
-from charms.opensearch.v0.helper_cos import (
-    _update_dashboard_title,
-    update_grafana_dashboards_titles,
-)
+from charms.opensearch.v0.helper_cos import update_grafana_dashboards_title
 
 
 class TestCOSGrafanaDashboard(unittest.TestCase):
 
     @patch("charms.opensearch.v0.helper_cos.get_charm_revision", return_value=167)
-    @patch("charms.opensearch.v0.helper_cos.Path.iterdir")
-    @patch("charms.opensearch.v0.helper_cos._update_dashboard_title")
-    def test_update_grafana_dashboards_titles(self, mock_update_dashboard, mock_iterdir, _):
-        mock_charm = MagicMock()
-        mock_charm.model.unit = MagicMock()
-        type(mock_charm).charm_dir = PropertyMock(return_value=Path("/fake/charm/dir"))
-
-        mock_json_file_1 = MagicMock(spec=Path)
-        mock_json_file_1.is_file.return_value = True
-        mock_json_file_1.suffix = ".json"
-        mock_json_file_1.name = "dashboard1.json"
-
-        mock_non_json_file = MagicMock(spec=Path)
-        mock_non_json_file.is_file.return_value = True
-        mock_non_json_file.suffix = ".txt"
-        mock_non_json_file.name = "not_a_dashboard.txt"
-
-        mock_json_file_2 = MagicMock(spec=Path)
-        mock_json_file_2.is_file.return_value = True
-        mock_json_file_2.suffix = ".json"
-        mock_json_file_2.name = "dashboard2.json"
-
-        mock_iterdir.return_value = [mock_json_file_1, mock_non_json_file, mock_json_file_2]
-
-        update_grafana_dashboards_titles(mock_charm)
-
-        # non-json files are not called
-        mock_update_dashboard.assert_has_calls(
-            [
-                call(167, mock_json_file_1),
-                call(167, mock_json_file_2),
-            ],
-            any_order=True,
-        )
-
-        self.assertEqual(mock_update_dashboard.call_count, 2)
-
-    @patch("charms.opensearch.v0.helper_cos.get_charm_revision", return_value=167)
-    @patch("charms.opensearch.v0.helper_cos.logger")
-    @patch("charms.opensearch.v0.helper_cos.Path.iterdir")
-    @patch("charms.opensearch.v0.helper_cos._update_dashboard_title")
-    def test_update_grafana_dashboards_titles_json_exception(
-        self, mock_update_dashboard, mock_iterdir, mock_logger, _
-    ):
-        mock_charm = MagicMock()
-        mock_charm.model.unit = MagicMock()
-        type(mock_charm).charm_dir = PropertyMock(return_value=Path("/fake/charm/dir"))
-
-        mock_json_file_1 = MagicMock(spec=Path)
-        mock_json_file_1.is_file.return_value = True
-        mock_json_file_1.suffix = ".json"
-        mock_json_file_1.name = "dashboard1.json"
-
-        mock_iterdir.return_value = [mock_json_file_1]
-
-        mock_update_dashboard.side_effect = json.JSONDecodeError("Error", "Error", 0)
-
-        update_grafana_dashboards_titles(mock_charm)
-
-        mock_logger.error.assert_called_once()
-
     @patch(
         "builtins.open",
         new_callable=mock_open,
         read_data=json.dumps({"title": "Charmed OpenSearch"}),
     )
     @patch("json.dump")
-    def test_update_dashboard_title_no_prior_revision(self, mock_json_dump, mock_open_func):
+    def test_update_grafana_dashboards_title_no_prior_revision(
+        self, mock_json_dump, mock_open_func, _
+    ):
+        mock_charm = MagicMock()
+        mock_charm.model.unit = MagicMock()
+        type(mock_charm).charm_dir = PropertyMock(return_value=Path("/fake/charm/dir"))
 
-        _update_dashboard_title(167, MagicMock())
+        update_grafana_dashboards_title(mock_charm)
 
         expected_updated_dashboard = {"title": "Charmed OpenSearch - Rev 167"}
         mock_json_dump.assert_called_once_with(
             expected_updated_dashboard, mock_open_func(), indent=4
         )
 
+    @patch("charms.opensearch.v0.helper_cos.get_charm_revision", return_value=167)
     @patch(
         "builtins.open",
         new_callable=mock_open,
         read_data=json.dumps({"title": "Charmed OpenSearch - Rev 166"}),
     )
     @patch("json.dump")
-    def test_update_dashboard_title_prior_revision(
+    def test_update_grafana_dashboards_title_prior_revision(
         self,
         mock_json_dump,
         mock_open_func,
+        _,
     ):
+        mock_charm = MagicMock()
+        mock_charm.model.unit = MagicMock()
+        type(mock_charm).charm_dir = PropertyMock(return_value=Path("/fake/charm/dir"))
 
-        _update_dashboard_title("167", MagicMock())
+        update_grafana_dashboards_title(mock_charm)
 
         expected_updated_dashboard = {"title": "Charmed OpenSearch - Rev 167"}
         mock_json_dump.assert_called_once_with(
             expected_updated_dashboard, mock_open_func(), indent=4
         )
 
+    @patch("charms.opensearch.v0.helper_cos.get_charm_revision", return_value=167)
     @patch(
         "builtins.open",
         new_callable=mock_open,
         read_data=json.dumps({"my-content": "content"}),
     )
     @patch("json.dump")
-    def test_update_dashboard_title_json_no_title(
+    def test_update_grafana_dashboards_title_json_no_title(
         self,
         mock_json_dump,
         mock_open_func,
+        _,
     ):
-
-        _update_dashboard_title("167", MagicMock())
+        mock_charm = MagicMock()
+        mock_charm.model.unit = MagicMock()
+        type(mock_charm).charm_dir = PropertyMock(return_value=Path("/fake/charm/dir"))
+        update_grafana_dashboards_title(mock_charm)
 
         expected_updated_dashboard = {
             "title": "Charmed OpenSearch - Rev 167",
