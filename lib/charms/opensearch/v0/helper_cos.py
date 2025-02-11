46e178de7fb039cffa60076a36d5cdac174d224a
Mehdi-Bendriss <bendrissmehdi@gmail.com>
2025-02-11 21:40:06 +0100

    Bump library version in help_cos

--- lib/charms/opensearch/v0/helper_cos.py
+++ lib/charms/opensearch/v0/helper_cos.py
@@ -9,6 +9,18 @@ from typing import TYPE_CHECKING
 
 from data_platform_helpers.version_check import get_charm_revision
 
+
+# The unique Charmhub library identifier, never change it
+LIBID = "98222ad783074ec6b098d4014b853119"
+
+# Increment this major API version when introducing breaking changes
+LIBAPI = 0
+
+# Increment this PATCH version before using `charmcraft publish-lib` or reset
+# to 0 if you are raising the major API version
+LIBPATCH = 1
+
+
 logger = logging.getLogger(__name__)
 
 if TYPE_CHECKING:
