92d036a75206b5ca5873be542db189cedb32c01c
phvalguima <pedro.guimaraes@canonical.com>
2025-02-19 15:25:31 +0100

    Update opensearch_backups.py

--- lib/charms/opensearch/v0/opensearch_backups.py
+++ lib/charms/opensearch/v0/opensearch_backups.py
@@ -536,6 +536,9 @@ class OpenSearchBackup(OpenSearchBackupBase):
 
     def _on_list_backups_action(self, event: ActionEvent) -> None:
         """Returns the list of available backups to the user."""
+        if not (deployment_desc := self.charm.opensearch_peer_cm.deployment_desc()):
+            event.fail("The action can be run only after the deployment is finished.")
+            return
         backups = {}
         try:
             backups = self._list_backups()
@@ -681,6 +684,9 @@ class OpenSearchBackup(OpenSearchBackupBase):
 
     def _on_restore_backup_action(self, event: ActionEvent) -> None:  # noqa #C901
         """Restores a backup to the current cluster."""
+        if not (deployment_desc := self.charm.opensearch_peer_cm.deployment_desc()):
+            event.fail("The action can be run only after the deployment is finished.")
+            return
         if self.charm.upgrade_in_progress:
             event.fail("Restore not supported while upgrade in-progress")
             return
@@ -748,6 +754,9 @@ class OpenSearchBackup(OpenSearchBackupBase):
 
     def _on_create_backup_action(self, event: ActionEvent) -> None:  # noqa: C901
         """Creates a backup from the current cluster."""
+        if not (deployment_desc := self.charm.opensearch_peer_cm.deployment_desc()):
+            event.fail("The action can be run only after the deployment is finished.")
+            return
         if self.charm.upgrade_in_progress:
             event.fail("Backup not supported while upgrade in-progress")
             return
