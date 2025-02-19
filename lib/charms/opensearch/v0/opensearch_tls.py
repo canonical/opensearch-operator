f1fa9f350600e306f226bdd98abf099c4c5d3463
phvalguima <pedro.guimaraes@canonical.com>
2025-02-19 15:29:50 +0100

    Update opensearch_tls.py

--- lib/charms/opensearch/v0/opensearch_tls.py
+++ lib/charms/opensearch/v0/opensearch_tls.py
@@ -101,7 +101,7 @@ class OpenSearchTLS(Object):
 
     def _on_set_tls_private_key(self, event: ActionEvent) -> None:
         """Set the TLS private key, which will be used for requesting the certificate."""
-        if not (deployment_desc := self.charm.opensearch_peer_cm.deployment_desc()):
+        if not self.charm.opensearch_peer_cm.deployment_desc():
             event.fail("The action can be run only after the deployment is finished.")
             return
         if self.charm.upgrade_in_progress:
