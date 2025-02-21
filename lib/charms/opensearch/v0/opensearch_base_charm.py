a636b4b6259e396a5763d437fa14b6d87ee82985
phvalguima <pedro.guimaraes@canonical.com>
2025-02-21 14:46:29 +0100

    Update lib/charms/opensearch/v0/opensearch_base_charm.py
    Co-authored-by: smethnani <82812166+smethnani@users.noreply.github.com>

--- lib/charms/opensearch/v0/opensearch_base_charm.py
+++ lib/charms/opensearch/v0/opensearch_base_charm.py
@@ -764,7 +764,7 @@ class OpenSearchBaseCharm(CharmBase, abc.ABC):
             event.fail("The action can only be run once the deployment is complete.")
             return
         if self.opensearch_peer_cm.deployment_desc().typ != DeploymentType.MAIN_ORCHESTRATOR:
-            event.fail("The action can be run only on the main cluster.")
+            event.fail("The action can only be run on the main cluster.")
             return        
         if not self.unit.is_leader():
             event.fail("The action can be run only on leader unit.")
