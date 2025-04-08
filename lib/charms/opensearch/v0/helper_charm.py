20dafb18a2f450c7b850cae4d1327db23eb5c4e5
smethnani <82812166+smethnani@users.noreply.github.com>
2025-04-08 07:04:24 +0000

    remove comment

--- lib/charms/opensearch/v0/helper_charm.py
+++ lib/charms/opensearch/v0/helper_charm.py
@@ -165,7 +165,6 @@ def all_units_names(charm: "OpenSearchBaseCharm") -> List[Unit]:
 
 def all_units(charm: "OpenSearchBaseCharm") -> List[Unit]:
     """Fetch the list of units for the current app."""
-    # if 0 planned units, return empty list
     return list(charm.model.get_relation(PeerRelationName).units.union({charm.unit}))
 
 
