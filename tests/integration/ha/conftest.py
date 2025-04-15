65dccf81574c41ca9d1a458dc270b8c691ab87f6
Smail Kourta <smail.kourta@canonical.com>
2025-04-15 13:52:35 +0000

    reverse c_write teardown

--- tests/integration/ha/conftest.py
+++ tests/integration/ha/conftest.py
@@ -27,10 +27,7 @@ async def reset_restart_delay(ops_test: OpsTest):
 async def c_writes(ops_test: OpsTest):
     """Creates instance of the ContinuousWrites."""
     app = (await app_name(ops_test)) or APP_NAME
-    c_writes = ContinuousWrites(ops_test, app)
-    yield c_writes
-    # stop
-    await c_writes.stop()
+    return ContinuousWrites(ops_test, app)
 
 
 @pytest.fixture(scope="function")
