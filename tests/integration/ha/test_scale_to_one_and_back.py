5afb23737149c151b7520adebde1602cf355561c
Smail Kourta <smail.kourta@canonical.com>
2025-04-15 11:55:50 +0000

    add scale to one and back

--- tests/integration/ha/test_scale_to_one_and_back.py
+++ tests/integration/ha/test_scale_to_one_and_back.py
@@ -13,7 +13,6 @@ from ..helpers import (
     APP_NAME,
     CONFIG_OPTS,
     MODEL_CONFIG,
-    SERIES,
     cluster_health,
     cluster_voting_config_exclusions,
     execute_update_status_manually,
@@ -37,14 +36,13 @@ logger = logging.getLogger(__name__)
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
 @pytest.mark.skip_if_deployed
-async def test_build_and_deploy(ops_test: OpsTest) -> None:
+async def test_build_and_deploy(ops_test: OpsTest, charm, ubuntu_base) -> None:
     """Build and deploy one unit of OpenSearch."""
     # it is possible for users to provide their own cluster for HA testing.
     # Hence, check if there is a pre-existing cluster.
     if await app_name(ops_test):
         return
 
-    my_charm = await ops_test.build_charm(".")
     # This test will manually issue update-status hooks, as we want to see the change in behavior
     # when applying `settle_voting` during start/stop and during update-status.
     MODEL_CONFIG["update-status-hook-interval"] = "360m"
@@ -57,7 +55,9 @@ async def test_build_and_deploy(ops_test: OpsTest) -> None:
         ops_test.model.deploy(
             TLS_CERTIFICATES_APP_NAME, channel=TLS_STABLE_CHANNEL, config=config
         ),
-        ops_test.model.deploy(my_charm, num_units=3, series=SERIES, config=CONFIG_OPTS),
+        ops_test.model.deploy(
+            charm, num_units=3, base=f"ubuntu@{ubuntu_base}", config=CONFIG_OPTS
+        ),
     )
 
     # Relate it to OpenSearch to set up TLS.
