a08ebb25dc3c5c03ce553f566de455117b4cdfb8
Smail Kourta <smail.kourta@canonical.com>
2025-04-23 06:12:28 +0000

    docstring fixes

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 import os
@@ -8,6 +7,7 @@ import pytest
 
 @pytest.fixture
 def ubuntu_base():
+    """charm base version to use for testing."""
     return os.environ["CHARM_UBUNTU_BASE"]
 
 
@@ -24,7 +24,7 @@ def series(ubuntu_base):
 
 @pytest.fixture
 def charm(ubuntu_base):
-    # TODO: use instead of ops_test.build_charm
+    """Path to the charm file to use for testing."""
     # Return str instead of pathlib.Path since python-libjuju's model.deploy(), juju deploy, and
     # juju bundle files expect local charms to begin with `./` or `/` to distinguish them from
     # Charmhub charms.
