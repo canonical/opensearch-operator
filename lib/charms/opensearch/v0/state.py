ed3e5b85e0c15017a0bb9fb86b6d0e65b0928792
smethnani <82812166+smethnani@users.noreply.github.com>
2025-10-21 09:17:15 +0400

    Add libid, libapi, libpatch to state.py

--- lib/charms/opensearch/v0/state.py
+++ lib/charms/opensearch/v0/state.py
@@ -26,6 +26,16 @@ from charms.opensearch.v0.opensearch_profile import (
     TestingProfile,
 )
 
+# The unique Charmhub library identifier, never change it
+LIBID = "7a9ae6df176c4147b898beaacdde4d2f"
+
+# Increment this major API version when introducing breaking changes
+LIBAPI = 0
+
+# Increment this PATCH version before using `charmcraft publish-lib` or reset
+# to 0 if you are raising the major API version
+LIBPATCH = 1
+
 logger = logging.getLogger(__name__)
 
 
