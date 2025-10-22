c0a43629cfc902e33e2cfa874a18dbffc23d4cf7
gatici <gulsum.atici@canonical.com>
2025-10-22 14:01:51 +0300

    fix: issues during merge conflict
    Signed-off-by: gatici <gulsum.atici@canonical.com>

--- lib/charms/opensearch/v0/opensearch_tls.py
+++ lib/charms/opensearch/v0/opensearch_tls.py
@@ -559,50 +559,14 @@ class OpenSearchTLS(Object):
             logging.error("CA cert  or truststore-password not found, quitting.")
             return False
 
-        store_path = f"{self.certs_path}/{CA_ALIAS}.p12"
-
-        try:
-            run_cmd(
-                f"""{self.keytool} -changealias \
-                -alias {CA_ALIAS} \
-                -destalias {OLD_CA_ALIAS} \
-                -keystore {store_path} \
-                -storetype PKCS12
-            """,
-                f"-storepass {admin_secrets.get('truststore-password')}",
-            )
-            logger.info(f"Current CA {CA_ALIAS} was renamed to old-{CA_ALIAS}.")
-        except OpenSearchCmdError as e:
-            # This message means there was no "ca" alias or store before, if it happens ignore
-            if not (
-                f"Alias <{CA_ALIAS}> does not exist" in e.out
-                or "Keystore file does not exist" in e.out
-            ):
-                raise
-
-        with tempfile.NamedTemporaryFile(
-            mode="w+t", dir=self.charm.opensearch.paths.conf
-        ) as ca_tmp_file:
-            ca_tmp_file.write(secrets.get("ca-cert"))
-            ca_tmp_file.flush()
-
-            try:
-                run_cmd(
-                    f"""{self.keytool} -importcert \
-                    -trustcacerts \
-                    -noprompt \
-                    -alias {CA_ALIAS} \
-                    -keystore {store_path} \
-                    -file {ca_tmp_file.name} \
-                    -storetype PKCS12
-                """,
-                    f"-storepass {admin_secrets.get('truststore-password')}",
-                )
-                run_cmd(f"sudo chmod +r {store_path}")
-                logger.info("New CA was added to truststore.")
-            except OpenSearchCmdError as e:
-                logging.error(f"Error storing the ca-cert: {e}")
-                return False
+        if not store_ca(
+            alias=CA_ALIAS,
+            store_pwd=admin_secrets.get("truststore-password"),
+            store_path=f"{self.certs_path}/{CA_ALIAS}.p12",
+            ca=secrets.get("ca-cert"),
+            keep_previous=True,
+        ):
+            return False
 
         self._add_ca_to_request_bundle(secrets.get("chain"))
 
