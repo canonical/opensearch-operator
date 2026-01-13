6687f9ba3801b328ea7dd1517462103302c91fa1
gatici <gulsum.atici@canonical.com>
2026-01-13 14:59:00 +0300

    chore: address review comments
    Signed-off-by: gatici <gulsum.atici@canonical.com>
    
    # Conflicts:
    #	.gitignore
    #	lib/charms/opensearch/v0/helper_charm.py
    #	lib/charms/opensearch/v0/helper_security.py
    #	lib/charms/opensearch/v0/opensearch_snapshots.py

--- lib/charms/opensearch/v0/helper_charm.py
+++ lib/charms/opensearch/v0/helper_charm.py
@@ -2,10 +2,12 @@
 # See LICENSE file for licensing details.
 
 """Utility functions for charms related operations."""
+import json
 import logging
 import os
 import re
 import subprocess
+from pathlib import Path
 from time import time_ns
 from types import SimpleNamespace
 from typing import TYPE_CHECKING, Iterable, List, Union
