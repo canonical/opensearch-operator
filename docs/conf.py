eb725002487a020f3a2c6622ec7365bbb7e56905
Vladimir <48120135+izmalk@users.noreply.github.com>
2026-01-15 22:00:44 +0000

    Comment out the slug option

--- docs/conf.py
+++ docs/conf.py
@@ -169,7 +169,7 @@ html_context = {
 # TODO: If your documentation is hosted on https://docs.ubuntu.com/,
 #       uncomment and update as needed.
 
-slug = 'charmed-opensearch'
+# slug = 'charmed-opensearch'
 
 #######################
 # Sitemap configuration: https://sphinx-sitemap.readthedocs.io/
