d072796fe2532363ca2751e79230d0f5065f549d
Vladimir <48120135+izmalk@users.noreply.github.com>
2026-01-22 22:28:52 +0000

    Add GA and cookie consent

--- docs/conf.py
+++ docs/conf.py
@@ -207,9 +207,13 @@ sitemap_excludes = [
 # Template and asset locations
 #######################
 
-#html_static_path = ["_static"]
-#templates_path = ["_templates"]
+html_static_path = [
+    ".sphinx/_static",
+]
 
+templates_path = [
+    ".sphinx/_templates",
+]
 
 #############
 # Redirects #
@@ -301,13 +305,15 @@ exclude_patterns = [
 
 # Adds custom CSS files, located under 'html_static_path'
 
-# html_css_files = []
-
+html_css_files = [
+    "cookie-banner.css",
+]
 
 # Adds custom JavaScript files, located under 'html_static_path'
 
-# html_js_files = []
-
+html_js_files = [
+    "bundle.js",
+]
 
 # Specifies a reST snippet to be appended to each .rst file
 
