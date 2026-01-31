52e84c94008c364ca62a537fbfb8c6ed4b472616
Mehdi-Bendriss <45567724+Mehdi-Bendriss@users.noreply.github.com>
2026-01-31 13:42:17 +0100

    fix linting

--- lib/charms/opensearch/v0/opensearch_distro.py
+++ lib/charms/opensearch/v0/opensearch_distro.py
@@ -393,9 +393,7 @@ class OpenSearchDistribution(ABC):
 
             if output.returncode != 0:
                 logger.debug(f"{command}:\n Stderr: {output.stderr}\n Stdout: {output.stdout}")
-                raise OpenSearchCmdError(
-                    cmd=command, out=output.stdout, err=output.stderr
-                )
+                raise OpenSearchCmdError(cmd=command, out=output.stdout, err=output.stderr)
         except (TimeoutError, subprocess.TimeoutExpired) as e:
             raise OpenSearchCmdError(cmd=command_with_args, err=str(e))
         return output.stdout.strip()
