aea0b59a59ed2bef9f848b5e664c159754441b60
Smail Kourta <smail.kourta@canonical.com>
2026-07-21 12:21:57 +0400

    patch: update metadata and update deps

--- src/charm.py
+++ src/charm.py
@@ -5,8 +5,23 @@
 
 """Charmed Machine Operator for OpenSearch."""
 
+import logging
+import os
+
+from opensearch_single_kernel.charms.k8s import OpenSearchK8sCharm
 from opensearch_single_kernel.charms.vm import OpenSearchVMCharm
 from ops.main import main
 
+logger = logging.getLogger(__name__)
+
+
+def is_k8s():
+    """Check if the charm is running in a Kubernetes environment."""
+    return "KUBERNETES_SERVICE_HOST" in os.environ
+
+
 if __name__ == "__main__":
-    main(OpenSearchVMCharm)
+    if is_k8s():
+        main(OpenSearchK8sCharm)
+    else:
+        main(OpenSearchVMCharm)
